8ebe40938d1835163cf791320f5eaeba9bd75b2c
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2023-11-23 12:16:37 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react"
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Routes from "./components/Routes"
+
+function App() {
+  const [darkTheme, setDarkTheme] = useState(false)
+
+  return (
+    <div className={darkTheme ? "dark" : "light"}>
+        <div className="bg-gray-100">
+        App
+        </div>
+    </div>
+     
+  )
+}
+
+export default App
